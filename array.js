0e5e30cf9009e8e1e766cd2806f8349b524e3490
Hifi58 <anaisbiquena@macbook-pro-anais.home>
2022-01-27 11:56:07 +0100

    first commit

---
+++ array.js
@@ -0,0 +1,22 @@
+let my_array = ['Pizza', 'Hamburger', 'Panini', 'Kebab']
+
+// Ajout fin de tableau
+my_array.push('Raclette')
+
+// Ajout début de tableau
+my_array.unshift('Hotdog')
+
+// Suppression fin de tableau
+my_array.pop()
+
+// Suppression début de tableau
+my_array.shift()
+
+// Suppression suivant la position dans le tableau
+my_array.splice(2)
+
+// Inserser un tableau
+my_array.reverse()
+
+// Connaitre l'index d'un élément
+my_array.indexOf('Pizza')
